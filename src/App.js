9a209e0e35a0ec8c72446f5ef8e097f4a2c90b42
Nitin Tembhare <nitintembhare340@gmail.com>
2023-07-04 16:00:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./component/Home/Home";
+import Detail from "./component/Details/Detail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/item/:id" element={<Detail />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
